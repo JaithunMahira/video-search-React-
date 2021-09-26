7ffab64c58bbb97581729c92a2376c57ff578950
JaithunMahira <jaithunmahira@gmail.com>
2021-09-26 20:23:40 +0530

    Refactoring using hooks and custom hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
-  onTermSubmit = async (searchString) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: searchString,
-      },
-    });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
+const App = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
+  const [videos, search ] = useVideos('buildings');
 
-  componentDidMount() {
-    this.onTermSubmit("buildings");
-  }
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  useEffect(() => {
+    setSelectedVideo(videos[0])
+  }, [videos]);
+  
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              onVideoSelect={setSelectedVideo}
+              videos={videos}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
---
+++ src/hooks/useVideos.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+const useVideos =(defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (searchString) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: searchString,
+      },
+    });
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+}
+
+export default useVideos;
